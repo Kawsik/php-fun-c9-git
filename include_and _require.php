fe1023cf3d523f65e55ba26ade51173577613657
ROY <roykawsik@gmail.com>
2016-06-19 02:35:29 +0000

    included and require

---
+++ include_and _require.php
@@ -0,0 +1,17 @@
+<?php include("included_header.php"); ?>
+    <!-- page can open with or without it -->
+<?php require("included_header.php"); ?>
+    <!-- page can not open without it -->
+<?php include_once("included_header.php"); ?>
+    <!-- page can open with or without it and calling that page only one time -->
+<?php require_once("included_header.php"); ?>
+    <!-- page can not without it and calling that page only one time -->
+
+
+
+
+
+<?php include("included_footer.php"); ?>
+<?php require("included_footer.php"); ?>
+<?php include_once("included_footer.php"); ?>
+<?php require_once("included_footer.php"); ?>
---
+++ included_header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Fun PHP</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <style type="text/css">
+        
+    </style>
+</head>
+<body>
+    <P>this is from included_header.php</P>
+    
